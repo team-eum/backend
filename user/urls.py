5d9d4526a8c4b1b56aa9a40ca84799b006be44c0
Jaewon Kim <sanvit@sanvit.me>
2025-01-19 02:37:21 +0900

    Implement user authentication and management APIs with serializers

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 urlpatterns = [
     path("social/<str:provider>",
          views.SocialAuthentication.as_view()),
+    path('signin', views.UserSignInAPIView.as_view()),
+    path('signup', views.UserSignUpAPIView.as_view()),
+    path('modify', views.UserModifyAPIView.as_view()),
+    path('sms-auth', views.SmsAuthAPIView.as_view()),
 ]
