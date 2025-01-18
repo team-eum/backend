b92150b23928ca5e6b75b3c673a96ee25d7c9751
Sullung <calzone0404@gmail.com>
2025-01-18 23:21:09 +0900

    fix open api key error

--- appointment/openai.py
+++ appointment/openai.py
@@ -1,11 +1,13 @@
-from openai import OpenAI, Stream
+from openai import OpenAI
 from openai.types.chat import ChatCompletion, ChatCompletionChunk
+from config.settings import OPENAI_API_KEY
+import config.settings
 
 
 class CustomOpenAI:
 
     def __init__(self):
-        self.client = OpenAI()
+        self.client = OpenAI(api_key=OPENAI_API_KEY)
 
     def get_summary(self, text) -> ChatCompletion:
         return self.client.chat.completions.create(
